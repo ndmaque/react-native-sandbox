5a6f04b4ab8fe2604cb2986cc7d6645080667bba
Andy McIntyre <ndmaque@gmail.com>
2018-05-10 12:48:38 +0100

    working. uses expo permissions

--- App.js
+++ App.js
@@ -1,116 +1,64 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, FlatList, Picker } from 'react-native';
-import { Constants } from 'expo';
-import { Rating, Button, Slider } from 'react-native-elements';
-import { Ionicons, FontAwesome } from '@expo/vector-icons';
-import {CheckBox} from 'react-native-check-box';
-import {RadioGroup, RadioButton} from 'react-native-flexi-radio-button'
+import { Platform, Text, View, StyleSheet } from 'react-native';
+import { Constants, Location, Permissions } from 'expo';
 
+// https://docs.expo.io/versions/latest/sdk/location
 
-export default class Test extends Component {
+export default class App extends Component {
+  state = {
+    location: null,
+    errorMessage: null,
+  };
 
-  constructor(props){
-    super(props);
-    this.state ={
-      initialRate: null,
-      rating: null,
-      age: null,
-      size: null,
-      latLon: null,
-      image: null
-    };
-    this.setRating = this.setRating.bind(this);
-    this.setAge = this.setAge.bind(this);
-    this.setSize = this.setSize.bind(this);
-    //this.setAge = this.setAge.bind(this);
-
+  componentWillMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this._getLocationAsync();
+    }
   }
 
-  setAge(index, value){
-    this.setState({age: value})
-  }
-
-  setRating(value) {
-    this.setState({rating: value})
-  }
-
-
-  setSize(index, value) {
-    this.setState({size: value})
-  }
-
-  saveTurd() {
-    const turd = {
-      age: this.state.age,
-      size: this.state.size,
-      rating: this.state.rating,
-      latLon: this.state.latLon,
-      image: this.state.image
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
     }
 
-    console.log('saveTurd', turd);
-    // create a payload to post to REST
-  }
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
+  };
 
   render() {
+    let text = 'Waiting..';
+    if (this.state.errorMessage) {
+      text = this.state.errorMessage;
+    } else if (this.state.location) {
+      text = JSON.stringify(this.state.location);
+    }
+
     return (
       <View style={styles.container}>
-        <Rating
-          startingValue={this.state.initialRate}
-          fractions={1}
-          type='heart'
-          ratingCount={5}
-          onFinishRating={this.setRating}
-          style={{ paddingVertical: 10 }}
-        />
-      <View>
-        <RadioGroup style={{flexDirection: 'row'}} onSelect={(index, value) => this.setAge(index, value)}>
-          <RadioButton value={'steaming'} >
-            <Text>Steaming</Text>
-          </RadioButton>
-          <RadioButton value={'recent'}>
-            <Text>Recent</Text>
-          </RadioButton>
-          <RadioButton value={'crusty'}>
-            <Text>Crusty</Text>
-          </RadioButton>
-        </RadioGroup>
-
-        <RadioGroup style={{flexDirection: 'row'}} onSelect={(index, value) => this.setSize(index, value)}>
-          <RadioButton value={'shihtzu'} >
-            <Text>Shitzu</Text>
-          </RadioButton>
-          <RadioButton value={'labradore'}>
-            <Text>Labrador</Text>
-          </RadioButton>
-          <RadioButton value={'dalmantion'}>
-            <Text>Dalmnation</Text>
-          </RadioButton>
-        </RadioGroup>
+        <Text style={styles.paragraph}>{text}</Text>
       </View>
-
-      <Button
-        onPress={() => this.saveTurd()}
-        title="Save Turd"
-      />
-      <Text>Rating: {this.state.rating}</Text>
-      <Text>Age: {this.state.age}</Text>
-      <Text>Size: {this.state.size}</Text>
-
-
-    </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
- flex: 1,
- paddingTop: 22
-},
-item: {
-  padding: 10,
-  fontSize: 18,
-  height: 44,
-},
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    textAlign: 'center',
+  },
 });
